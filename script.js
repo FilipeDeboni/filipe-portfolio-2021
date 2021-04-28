3bc953ca6b6d11e2e6b17ecc78ad7267aaee34c0
Filipe <deboni.filipe@gmail.com>
2021-04-28 18:42:46 -0300

    basic structure of home/projects/about/contact done

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ $(document).ready(function(){
     }else{
       $('.navbar').removeClass("sticky");
     }
+    if(this.scrollY > 500){
+      $('.scroll-up-btn').addClass("active");
+    }else{
+      $('.scroll-up-btn').removeClass("active");
+    }
+  });
+
+  // slide up script
+  $('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0})
   });
 
   // Toggle menu/navbar script
@@ -13,3 +23,18 @@ $(document).ready(function(){
     $('.menu-btn i').toggleClass("active");
   });
 });
+
+// Typing Animation Script
+  var typed = new Typed(".typing", {
+    strings: ["Front-End Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: ["Front-End Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
